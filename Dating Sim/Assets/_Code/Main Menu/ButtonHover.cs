38f1c4bc19fff20322452d2c29c3de7ec72f78ba
Iki Pham <dev.ikiguise@gmail.com>
2023-02-13 11:07:17 -0600

    menu text

--- Dating Sim/Assets/_Code/Main Menu/ButtonHover.cs
+++ Dating Sim/Assets/_Code/Main Menu/ButtonHover.cs
@@ -6,14 +6,53 @@ using UnityEngine.EventSystems;
 public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject buttonBorder;
+    public GameObject buttonText;
+    public float timerDuration = 5f;
+    private float timerStartTime;
+    private bool buttonActive;
+
+    void Start()
+    {
+        timerStartTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time - timerStartTime >= timerDuration)
+        {
+            SetTextActive();
+            StartCoroutine(WaitForTime());
+
+            if (buttonActive == true)
+            {
+                buttonText.SetActive(false);
+                buttonActive = false;
+            }
+
+            timerStartTime = Time.time;
+        }
+    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         buttonBorder.SetActive(true);
+        buttonText.SetActive(true);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         buttonBorder.SetActive(false);
+        buttonText.SetActive(false);
+    }
+
+    public void SetTextActive()
+    {
+        buttonText.SetActive(true);
+        buttonActive = true;
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(10);
     }
 }
