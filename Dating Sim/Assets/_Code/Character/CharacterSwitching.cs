f4f8624ced32c55db7f23f8e40eb9730d23518af
WEBSTERAD\wustudent <dts@webster.edu>
2023-02-15 13:40:35 -0600

    sdafjkso[flsa[fsd;da

---
+++ Dating Sim/Assets/_Code/Character/CharacterSwitching.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSwitching : MonoBehaviour
+{
+    public Actors[] actorsList;
+
+    void Awake()
+    {
+        for each
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
