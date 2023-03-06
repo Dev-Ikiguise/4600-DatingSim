d3830aaaf41eb6a49489f15c5f23997d24145405
Iki Pham <dev.ikiguise@gmail.com>
2023-03-05 22:51:35 -0600

    Inventory System

--- Dating Sim/Assets/_Code/GameUI/Hotbar.cs
+++ Dating Sim/Assets/_Code/GameUI/Hotbar.cs
@@ -4,13 +4,65 @@ using UnityEngine;
 
 public class Hotbar : MonoBehaviour
 {
-    void Start()
+    public GameObject phoneElement;
+    public GameObject hotbarElement;
+    public GameObject showHotbarElement;
+    private bool gamePaused = false;
+    private bool hotbarActive = true;
+
+    private void Start()
     {
-        
+        showHotbarElement.SetActive(false);
+        phoneElement.SetActive(false);
     }
 
     void Update()
     {
-        
+        // Toggle the UI element on button press
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePhone()
+    {
+        if (phoneElement != null)
+        {
+            phoneElement.SetActive(!phoneElement.activeSelf);
+        }
+    }
+
+    public void ToggleHotbar()
+    {
+        if (hotbarActive == false)
+        {
+            hotbarElement.SetActive(true);
+            showHotbarElement.SetActive(false);
+            hotbarActive = true;
+        }
+        else if (hotbarActive == true)
+        {
+            hotbarElement.SetActive(false);
+            showHotbarElement.SetActive(true);
+            hotbarActive = false;
+        }
+    }
+
+    public void TogglePause()
+    {
+        // Toggle the game's pause state
+        if (!gamePaused)
+        {
+            Time.timeScale = 0f; // Pause the game
+            gamePaused = true;
+            Debug.Log("Paused");
+        }
+        else
+        {
+            Time.timeScale = 1f; // Resume the game
+            gamePaused = false;
+            Debug.Log("Unpaused");
+        }
     }
 }
