f1b1d91586e5bf794e9509b49463c9a7ce47592b
WUStudent <wustudent@webster.edu>
2023-03-29 17:26:12 -0500

    jdkslflsjlsadjkdjfajhfasdfjkjhsdfjhka

---
+++ Dating Sim/Assets/_Code/Managers/InventoryManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance;
+    public List<Item> Items = new List<Item>();
+
+    public Transform ItemContent;
+    public GameObject InventoryItem;
+
+    public InventoryItemController[] InventoryItems;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Add(Item item)
+    {
+        Items.Add(item);
+    }
+
+    public void Remove(Item item)
+    {
+        Items.Remove(item);
+    }
+
+    public void ListItems()
+    {
+        foreach (Transform item in ItemContent)
+        {
+            Destroy(item.gameObject);
+        }
+
+        foreach (var item in Items)
+        {
+            GameObject obj = Instantiate(InventoryItem, ItemContent);
+            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+
+            itemIcon.sprite = item.icon;
+        }
+
+        SetInventoryItems();
+    }
+
+    public void SetInventoryItems()
+    {
+        InventoryItems = ItemContent.GetComponentsInChildren<InventoryItemController>();
+
+        for (int i = 0; i < Items.Count; i++)
+        {
+            InventoryItems[i].AddItem(Items[i]);
+        }
+    }
+}
